03bea5444c2557a2d8481629c7d35ff1db04a6e3
kimmeh <mhko0411@gmail.com>
2023-08-17 21:45:04 +0900

    fix baspath

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: "/",
+  basePath: "/https://kimmihi.github.io/",
 };
 
 module.exports = nextConfig;
