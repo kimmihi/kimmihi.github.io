1c8a8eac434a6df3053f0aedf8dab4512d5ab923
kimmeh <mhko0411@gmail.com>
2023-08-17 21:56:46 +0900

    fix nextconfig

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
+const debug = process.env.NODE_ENV !== "production";
 const nextConfig = {
   reactStrictMode: true,
-  basePath: "/",
+  assetPrefix: !debug ? `/` : "",
 };
 
 module.exports = nextConfig;
